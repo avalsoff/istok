0bdd60ac34fc94987a88c8fcdfd1462e0a54fba5
avalsoff <avalsoff@gmail.com>
2018-06-29 11:18:19 +1000

    Restyle disclaimer

--- src/views/Disclaimer.vue
+++ src/views/Disclaimer.vue
@@ -1,28 +1,27 @@
 <template>
 <v-touch 
   @swipeleft="goToCard">
-  <div class="card">
-    <span class="hashtag">#50вопросов_исток</span>
-    <span class="bullets">&bull; &bull; &bull; </span>
-    <div class="text-wrapper">
-      <p class="text">
-        {{ disclaimer[0] }}
-      </p>
-      <p class="text">
-        {{ disclaimer[1] }}
-      </p>
-      <p class="text">
-        {{ disclaimer[2] }}
-      </p>
-      <p class="text">
-        {{ disclaimer[3] }}        
-      </p>
-      <em class="text em">
-        {{ disclaimer[4] }}        
-      </em>
-    </div>
-    <div class="logo">
-      <img class="logo-img" src="../assets/logo-disclaimer.png" alt="Тренинг центр - Исток">
+  <div class="wrapper">
+    <div class="disclaimer">
+      <span class="disclaimer__hashtag">#50вопросов_исток</span>
+      <div class="disclaimer__text-wrapper">
+        <p class="disclaimer__text">
+          {{ disclaimer[0] }}
+        </p>
+        <p class="disclaimer__text">
+          {{ disclaimer[1] }}
+        </p>
+        <p class="disclaimer__text">
+          {{ disclaimer[2] }}
+        </p>
+        <p class="disclaimer__text">
+          {{ disclaimer[3] }}        
+        </p>
+        <p class="disclaimer__text">
+          {{ disclaimer[4] }}        
+        </p>
+      </div>
+      <router-link class="disclaimer__go-to-cards" tag="a" to="/card">Далее</router-link>
     </div>
   </div>
 </v-touch>
@@ -39,9 +38,6 @@ export default {
     disclaimer: state => state.disclaimer
   }),
   methods: {
-    // getDisclamerPart(i) {
-    //   return this.$store.state.disclaimer[i];
-    // },
     goToCard() {
       this.$router.push('card');
     }
@@ -49,135 +45,58 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.bullets {
-  display: block;
-  line-height: 70px;
-  font-size: 75px;
-  font-family: Arial;
-  text-align: center;
-}
+<style lang="scss" scoped>
+@import "../scss/mixins";
 
-.card {
-  position: relative;
-  font-family: 'Geometria Light', Arial, Helvetica, sans-serif;
-  overflow: hidden;
-  height: 100vh;
-  /* vh */
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 800px;
-  background-color: #F6751C;
+.wrapper {
+  font-size: 14px;
+  font-family: "Geometria", Arial, Helvetica, sans-serif;
   color: #fff;
-  /* border: 1px solid #000; */
+  background-color: #EA7D00;
+  padding: 30px 35px;
+  background-repeat: no-repeat;
+  background-image: url('../assets/istok-white.png');
+  background-position: 50% 94%;
 }
 
-.hashtag {
-  display: block;
-  text-align: center;
-  font-size: 32px;
-  line-height: 58px;
-  padding: 0 50px;
-  margin-top: 7%;
-}
-
-.text-wrapper {
-  position: relative;
-  padding: 0 100px;
-  height: 78vh;
-  /* vh */
-}
-
-.text {
-  margin-top: 0;
-  margin-bottom: 10px;
-  margin-right: auto;
-  margin-left: auto;
-  font-size: 28px;
-  line-height: 48px;
-  width: 100%;
-  max-width: 800px;
-  text-align: left;
-  text-indent: 40px;
-  /* border: 1px solid #000; */
-}
-
-.logo {
-  overflow: hidden;
-  z-index: 99;
-  position: absolute;
-  top: 85%;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 450px;
-}
-
-.logo-img {
-  width: 100%;
-  height: auto;
-}
-
-@media only screen and (max-width: 700px) {
-  .hashtag {
-    font-size: 30px;
-    line-height: 35px;
-    padding: 0 50px;
-    margin-bottom: 10px;
+.disclaimer {
+  &__hashtag {
+    font-family: "Geometria Medium", Arial, Helvetica, sans-serif;
   }
 
-  .text-wrapper {
-    padding: 0 10px 0 30px;
+  &__text-wrapper {
+    margin-top: 20px;
   }
 
-  .text {
-    font-size: 25px;
-    line-height: 26px;
-    width: 100%;
-    /* border: 1px solid #000; */
+  &__text {
+    font-family: "Geometria Light", Arial, Helvetica, sans-serif;
+    line-height: 21px;
+    text-indent: -4px;
   }
 
-  .bullets {
-    font-size: 45px;
-    line-height: 60px;
-  }
-
-  .logo {
-    width: 300px;
-  }
-
-  .text.em {
-    display: block;
-    margin-top: 30px;
-    text-indent: 0;
+  &__go-to-cards {
+    position: relative;
+    display: inline-block;
+    margin-top: 5px;
+    color: #fff;
+    text-decoration: none;
+    font-family: "Geometria Medium", Arial, Helvetica, sans-serif;
+    
+    &::after {
+      position: absolute;
+      top: -2px;
+      right: -32px;
+      content: "";
+      width: 24px;
+      height: 24px;
+      background-color: #fff;
+      border-radius: 50%;
+      background-image: url("../assets/arrow-right.svg");
+      background-repeat: no-repeat;
+      background-size: 40%;
+      background-position: 50% 50%;
+    }
   }
 }
 
-@media only screen and (max-width: 400px) {
-  .hashtag {
-    font-size: 24px;
-    line-height: 30px;
-    padding: 0 50px;
-    margin-bottom: 0;
-  }
-
-  .text-wrapper {
-    padding: 0 15px 0 25px;
-  }
-
-  .text {
-    font-size: 20px;
-    line-height: 20px;
-    width: 100%;
-    /* border: 1px solid #000; */
-  }
-
-  .bullets {
-    font-size: 35px;
-    line-height: 40px;
-  }
-  .logo {
-    width: 250px;
-  }
-}
 </style>
